927eef74453e5963386f001402ba6d0f8f47dd81
Bachrul Uluum <Bachrul Uluum>
2022-11-22 15:20:25 +0800

    mengubah desain beranda

--- src/Home.jsx
+++ src/Home.jsx
@@ -2,12 +2,12 @@ import ButtonHome from "./Components/ButtonHome";
 
 function Home() {
     return (
-        <div className="mx-auto max-w-md bg-primary h-screen">
-            <div className="h-1/2 bg-[url('./bg_home.png')] p-4">
+        <div className="mx-auto max-w-md bg-[url('./bg_home.png')] h-screen">
+            <div className="h-1/2  p-4">
                 <h1 className="text-3xl font-bold text-white">Beranda</h1>
                 {/* <img src="./bg_home.png" alt="bg_home.png" /> */}
             </div>
-            <div className="h-1/2 p-4 grid ">
+            <div className="h-1/2 p-4 grid bg-primary rounded-t-xl">
                 <ButtonHome Text="Pesan Kursi" />
                 <ButtonHome Text="Pesan Makanan" />
                 <ButtonHome Text="Pesan Minuman" />
